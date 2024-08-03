d8c79e48b03c3e42406df66cc39a4648fb015a69
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-08-03 21:43:22 +0400

    "Added clean, as well as HTML Template from webpack"

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const { type } = require('os');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'workit-kit',
+        }),
+    ],
     module: {
         rules: [
             {
