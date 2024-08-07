11b447462d7d07de318cb845d307dc95d59aa871
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-08-07 15:24:38 +0400

    "Re-added /dist to gitignore, created and rigorously tested task-master.js's functionalities. In process of linking task-master and dom-master. Dom-master structure changed for ease of use"

--- src/components/task-master.js
+++ src/components/task-master.js
@@ -26,33 +26,30 @@ class Subtask {
 //super duper crucial
 const projects = {};
 
-//adding example tasks - without polluting names
-(function(){
-    const workCat = new Category("Work Tasks", "This category contains all tasks pertaining to work");
+const workCat = new Category("Work Tasks", "This category contains all tasks pertaining to work");
 
-    projects.workCat = workCat;
+projects.workCat = workCat;
 
-    function exampleTasks() {
+const exampleTasksObj = {
+    addExampleTasks: ()=> {
         const exampleTask1 = new Task("Get Followers", "Run a social media campaign to generate new followers", '31/08/2024', 'high');
 
         //either move this to own object, or do something else
-        subtasksObj.exampleSubtasks1(exampleTask1)
+        exampleSubtasksObj.subtasks1(exampleTask1)
 
         const exampleTask2 = new Task("Market Research", "Conduct a market research on our client's company, to better understand the client Avatar", "20/08/2024", 'high');
 
-        subtasksObj.exampleSubtasks2(exampleTask2);
+        exampleSubtasksObj.subtasks2(exampleTask2);
 
         const exampleTask3 = new Task("Find a lead-magnet", 'Find or create a lead magnet that can help generate more traffic to our website', '25/08/2024', 'low');
 
         const exampleTask4 = new Task("Instagram Posts", 'Start making instagram posts for our Instagram account V and Bruno', 'today', 'medium');
 
         workCat.tasks.push(exampleTask1, exampleTask2, exampleTask3, exampleTask4);
-    };
+    }
+}
 
-    exampleTasks();
-})();
-
-const subtasksObj = {
+const exampleSubtasksObj = {
     subtasks1: (exampleTask1) => {
         const subtask1 = new Subtask("Create a buget");
         const subtask2 = new Subtask("Determine project targets");
@@ -69,4 +66,5 @@ const subtasksObj = {
     },
 }
 
+exampleTasksObj.addExampleTasks()
 export { Task, Category, Subtask, projects};
