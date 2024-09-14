fa4d8d2268390fcb718bc0613ac8374e8a77f93e
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-09-14 20:39:42 +0400

    "Final final commit, fixed img"

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { projects } from './components/task-master.js';
 import {domManager, domMain} from './components/dom-master.js';
 import { changeListener } from './components/sub-to-changes.js';
 changeListener.loadChanges()
+import vAndBrunoLogo from './assets/images/v-and-bruno.png';
+
 
 domManager.findDom();
 //keep the default load as whatever for now, but change it to today or soon - looks better
 domMain.defaultLoad("Work Tasks")
 
-console.log(projects) 
+document.getElementById('v-and-bruno-logo').src = vAndBrunoLogo;
